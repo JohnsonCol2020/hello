36cd5838834a85b0cf78486cf89403f466a9be4d
JohnsonCol2020 <cjohnson184@huskers.unl.edu>
2020-08-20 00:40:03 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ *  Author: Colton Johnson
+ *  Date: 2020/08/15
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+    printf("Colton Johnson!\n");
+
+    return 0;
+}
